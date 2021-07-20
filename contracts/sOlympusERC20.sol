6e4ae8d5a8ab01c72e61aed823eb682390d72c92
JExtor <JeffreyJExtor@gmail.com>
2021-07-19 23:13:27 -0400

    wrote deployment script that will deploy Version 1.1 of Olympus, everything except LP tokens and bonds are deployed. Changed every instance of 'valueOf' in Treasury to 'valueOfToken', this solidity function was coflicting w js object property name

--- contracts/sOlympusERC20.sol
+++ contracts/sOlympusERC20.sol
@@ -1018,7 +1018,7 @@ contract sOlympus is ERC20Permit, Ownable {
     mapping( address => bool ) public cannotReceive;
 
     uint256 private constant MAX_UINT256 = ~uint256(0);
-    uint256 private constant INITIAL_FRAGMENTS_SUPPLY = 500000 * 10**9;
+    uint256 private constant INITIAL_FRAGMENTS_SUPPLY = 5000000 * 10**9;
 
     // TOTAL_GONS is a multiple of INITIAL_FRAGMENTS_SUPPLY so that _gonsPerFragment is an integer.
     // Use the highest value that fits in a uint256 for max granularity.
